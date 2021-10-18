dc9d252ba164b425626090fa15992f2f72809175
salrey <tekdehn@gmail.com>
2021-10-18 11:15:52 -0400

    Clear console, add error message

--- letsdothis.js
+++ letsdothis.js
@@ -1,8 +1,14 @@
 const readlineSync = require('readline-sync')
 const getMenu = require('./getMenu')
 
+//lets clear the terminal instead of having the menu repeat multiple times - console.clear just moves up it doesn't clear, so add this to the loop as well
+console.clear()
+
 let response = readlineSync.question(getMenu())
 
-while (response !== "q") {
+while (response.toLowerCase() !== "q") {
+    console.log('Invalid input: ' + response)
     response = readlineSync.question(getMenu());
+    console.clear()
 }
+
