187e90c2a47d4eee0938e237de802c63aea978ca
salrey <tekdehn@gmail.com>
2021-10-18 12:44:17 -0400

    Print new todos in terminal by mapping array of objects'

--- getMenu.js
+++ getMenu.js
@@ -1,15 +1,30 @@
 const chalk = require('chalk')
+const todos = require('./todos')
 
 const getMenu = () => {
     const menu = `${chalk.bold.blue('Choose a todo to toggle its completeness (by entering its number) or choose an option at the bottom (by letter): ')}
-    1. ✅ Walk the dog.
-    2. ❎ Do the dishes.
-    3. ✅ Call the bank.
-    4. ❎ Sign up for class.
-    (l) Add new todo.
-    (q) Quit
+${todos.map((todo, i) => {
+    let symbol = '❎'
+    if (todo.isComplete) {
+        symbol = "✅"
+    }
+
+    return `${i + 1}. ${symbol} ${todo.text}`
+}).join('\n')}
+(n) Add new todo.
+(q) Quit
     `
     return menu
 }
 
 module.exports = getMenu;
+
+// function map(arr, callback) {
+//     const newArr = []
+//     for (let i =0; i < arr.length; i++) {
+//         newArr.push(element[i], i)
+//     }
+//     return newArr
+// }
+
+// map(numbers, (x) => x*2)
