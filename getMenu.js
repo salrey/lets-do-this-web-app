89d4204c43acdcf4ac5c01d26aa8d6822bd14163
salrey <tekdehn@gmail.com>
2021-10-18 11:24:02 -0400

    Add chalk colors

--- getMenu.js
+++ getMenu.js
@@ -1,5 +1,7 @@
+const chalk = require('chalk')
+
 const getMenu = () => {
-    const menu = `Choose a todo to toggle its completeness (by entering its number) or choose an option at the bottom (by letter):
+    const menu = `${chalk.bold.blue('Choose a todo to toggle its completeness (by entering its number) or choose an option at the bottom (by letter): ')}
     1. ✅ Walk the dog.
     2. ❎ Do the dishes.
     3. ✅ Call the bank.
--- letsdothis.js
+++ letsdothis.js
@@ -1,13 +1,13 @@
 const readlineSync = require('readline-sync')
+const chalk = require('chalk')
 const getMenu = require('./getMenu')
 
-//lets clear the terminal instead of having the menu repeat multiple times - console.clear just moves up it doesn't clear, so add this to the loop as well
 console.clear()
 
 let response = readlineSync.question(getMenu())
 
 while (response.toLowerCase() !== "q") {
-    console.log('Invalid input: ' + response)
+    console.log(chalk.red.bold('Invalid input: ' + response))
     response = readlineSync.question(getMenu());
     console.clear()
 }
