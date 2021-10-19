f117b3066a7288e00326fac1cf79f2dfdc2bf5c6
salrey <tekdehn@gmail.com>
2021-10-19 11:40:59 -0400

    Delete and edit text todo

--- editToDo.js
+++ editToDo.js
@@ -14,12 +14,23 @@ ${symbol} ${todo.text}
 (e) Edit the text of the todo.
 (x) Delete the todo.
 `
+//Implementing CRUD
     const response = readlineSync.question(editMenu)
     if (response === 'u') {
         todo.isComplete = false;
     } else if (response === 'd') {
         todo.isComplete = true;
-    }    
+    } else if (response === 'e') {
+        console.clear()
+        todo.text = readlineSync.question('What should we change the text to?\n\n')
+    } else if (response === "x") {
+        todos.forEach((element, i) => {
+            if (element.text === todo.text) {
+                //remove it using splice since you know the index
+                todos.splice(i, 1)
+            }
+        })
+    } 
     console.clear();
 }
 
